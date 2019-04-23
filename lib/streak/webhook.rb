e8c9ff4f580e774818bca094668a151d8fb45357
Conrad Chu <conradchu@conradchu.com>
2019-04-23 00:34:10 -0700

    Adds webhook support

---
+++ lib/streak/webhook.rb
@@ -0,0 +1,29 @@
+module Streak
+  class Webhook < StreakObject
+    def self.all_pipeline_webhooks(key)
+      res = Streak.request(:get, "/v2/pipelines/#{key}/webhooks")
+      convert_to_streak_object(res, Webhook)
+    end
+
+    def self.all_team_webhooks(key)
+      res = Streak.request(:get, "/v2/teams/#{key}/webhooks")
+      convert_to_streak_object(res, Webhook)
+    end
+
+    def self.find(key)
+      res = Streak.request(:get, "/v2/webhooks/#{key}")
+      convert_to_streak_object(res, Webhook)
+    end
+
+    def self.create(params)
+      res = Streak.request(:put, "/v2/webhooks", params)
+      convert_to_streak_object(res, Webhook)
+    end
+
+    def self.update(key, params)
+      res = Streak.request(:post, "/v2/webhooks/#{key}", MultiJson.dump(params))
+      convert_to_streak_object(res, Webhook)
+    end
+  end
+end
+
