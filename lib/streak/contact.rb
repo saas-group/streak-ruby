bbc5747e71b72b3e3939a9175175944883c7b59c
Tobias Schlottke <ts@omr.com>
2021-02-16 11:36:24 +0100

    contacts #3

--- lib/streak/contact.rb
+++ lib/streak/contact.rb
@@ -1,17 +1,17 @@
 module Streak
   class Contact < StreakObject
     def self.find(key)
-      res = Streak.request(:get, "/v2/contacs/#{key}")
+      res = Streak.request(:get, "/v2/contacts/#{key}")
       convert_to_streak_object(res, Comment)
     end
 
     def self.create(key, params, api_key=nil)
-      res = Streak.request(:post, "/v2/contacs/#{key}", MultiJson.dump(params), api_key)
+      res = Streak.request(:post, "/v2/contacts/#{key}", MultiJson.dump(params), api_key)
       convert_to_streak_object(res, Comment)
     end
 
     def self.update(key, params, api_key=nil)
-      res = Streak.request(:post, "/v2/contacs/#{key}", MultiJson.dump(params), api_key)
+      res = Streak.request(:post, "/v2/contacts/#{key}", MultiJson.dump(params), api_key)
       convert_to_streak_object(res, Comment)
     end
   end
