c9e0ce9b7ffa89ae0b7d17cc375345fc5c3415e4
Tobias Schlottke <ts@omr.com>
2021-02-16 12:44:18 +0100

    to_s fix for contacts create

--- lib/streak/contact.rb
+++ lib/streak/contact.rb
@@ -6,7 +6,7 @@ module Streak
     end
 
     def self.create(teamKey, params, get_if_existing=true, api_key=nil)
-      res = Streak.request(:post, "/v2/teams/#{teamKey}/contacts/?getIfExisting="+get_if_existing, MultiJson.dump(params), api_key)
+      res = Streak.request(:post, "/v2/teams/#{teamKey}/contacts/?getIfExisting="+get_if_existing.to_s, MultiJson.dump(params), api_key)
       convert_to_streak_object(res, Comment)
     end
 
