e5a6e62bcd009a48217241df4783f02fb893c99a
Tobias Schlottke <ts@omr.com>
2021-02-16 12:10:50 +0100

    finish creating contacts

--- lib/streak/contact.rb
+++ lib/streak/contact.rb
@@ -5,8 +5,8 @@ module Streak
       convert_to_streak_object(res, Comment)
     end
 
-    def self.create(key, params, api_key=nil)
-      res = Streak.request(:post, "/v2/contacts/#{key}", MultiJson.dump(params), api_key)
+    def self.create(teamKey, params, api_key=nil)
+      res = Streak.request(:post, "/v2/teams/#{teamKey}/contacts/", MultiJson.dump(params), api_key)
       convert_to_streak_object(res, Comment)
     end
 
